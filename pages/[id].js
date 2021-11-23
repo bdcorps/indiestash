076fce9bb6028458e50f287d2d23ff7aafc7ce8d
Sukh <sunnyashiin@gmail.com>
2021-11-23 13:31:52 -0500

    add structured data

--- pages/[id].js
+++ pages/[id].js
@@ -1,10 +1,59 @@
 import { booklists } from '../data'
 import { getBooklists } from './api/booklists/[id]'
-import Meta from '../components/Meta'
+// import Meta from '../components/Meta'
+import { NextSeo } from 'next-seo';
+import { ArticleJsonLd } from 'next-seo';
+import * as moment from 'moment';
+
 
 const article = ({ booklists, title, updatedAt }) => {
+  let url = ""
+  if (typeof window !== 'undefined') {
+    url = window.location.href;
+  }
+
+  const allImages = booklists.map(i => i.volumeInfo.imageLinks?.thumbnail).filter(i => !!i);
+
   return (<>
-    <Meta title={title} />
+    {/* <Meta title={title} /> */}
+
+    <NextSeo
+      title={title}
+      description={title}
+      openGraph={{
+        url,
+        title,
+        description: title,
+        images: [
+          {
+            url: 'https://saasbase.dev/content/images/2021/07/saasbase-open-graph-1.png',
+            width: 800,
+            height: 600,
+            alt: 'Title Image',
+            type: 'image/jpeg',
+          }
+        ],
+        site_name: 'SaaSBase',
+      }}
+      twitter={{
+        site: '@sssaini_',
+        cardType: 'summary_large_image',
+      }}
+    />
+
+    <ArticleJsonLd
+      url={url}
+      title={title}
+      images={allImages}
+      datePublished={moment().format()}
+      dateModified={moment().format()}
+      authorName={['SaaSBase']}
+      publisherName="SaaSBase"
+      publisherLogo="https://lh3.googleusercontent.com/XtsdnN74E96NAe4U1ffj1pE7dUNtc0e85ae0mMaBt3CRmdV3INXyHco6m4j9MxO2ylvACOuvwXI"
+      description={title}
+    />
+
+
     <header className="pt-6 xl:pb-10">
       <dd className="text-base leading-6 font-medium text-gray-500"><time dateTime="2021-02-16T16:05:00.000Z">Tuesday, Febuary 16, 2021</time></dd>
       <h1 className="mt-2 text-3xl leading-9 font-bold text-gray-900 tracking-tight sm:text-4xl sm:leading-10 md:text-5xl md:leading-14">
